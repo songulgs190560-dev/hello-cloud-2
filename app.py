734de86d87336d286565c161efffc49318b69310
songulgs190560-dev <songulgs190560@gmail.com>
2026-01-02 16:23:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,23 @@ from flask import Flask, render_template_string, request
 import os
 import psycopg2
 
-app=Flask (__name__
+app=Flask (__name__)
 
-DATABASE_URL = os.getentv("DATABASE_URL"," ")
+DATABASE_URL = os.getenv("DATABASE_URL"," ")
 
-HTML = " " "
+HTML = """
 <!doctype html>
 <html>
 <head>
    <title>Buluttan Selam!</title>
    <style>
-        body  {  fony-family: Arial; text_align: center; padding: 50px; backgraund: #eef2f3; }
+        body  {  font-family: Arial; text-align: center; padding: 50px; background: #eef2f3; }
         h1    {  color:  #333;  }
         form  {  margin:  20px auto;  }
         input {padding:  10px; font-size:  16px;  }
-        button {padding:  10px  15px; backgraund:  #4CAF50; color:white border: none; border-radius: 6px; cursor: pointer; }
+        button {padding:  10px  15px; background:  #4CAF50; color:white; border: none; border-radius: 6px; cursor: pointer; }
         ul  { list-style: none;  padding: 0;  }
-        li  { backgraund: white; margin: 5px auto; width: 200px; padding: 8px; border-radius: 5px; }
+        li  { background: white; margin: 5px auto; width: 200px; padding: 8px; border-radius: 5px; }
    </style>
 </head>
 <body>
@@ -29,7 +29,7 @@ HTML = " " "
             </form>
             <h3>ziyaretçiler:</h3>
             <ul>
-               {% for ad in isimler % }
+               {% for ad in isimler %}
                  <li>{{ ad }}</li>
                {% endfor %}
            </ul>
@@ -45,24 +45,24 @@ def connect_db():
 def index():
   conn = connect_db()
   cur = conn.cursor()
-  cur.execute=("CREATE TABLE IF NOT EXISTS ziyaretçiler (id SERIAL PRIMARY KEY, isim TEXT)")
+  cur.execute("CREATE TABLE IF NOT EXISTS ziyaretciler (id SERIAL PRIMARY KEY, isim TEXT)")
 
 
   if request.method=="POST":
-     isim=request.from.get("isim")
+     isim = request.form.get("isim")
      if isim:
-       cur.execute("INSERT INTO ziyaretçiler (isim) VALUES (%s)", (isim,))
+       cur.execute("INSERT INTO ziyaretciler (isim) VALUES (%s)", (isim,))
        conn.commit()
 
-       cur.execute("SELECT isim FROM ziyaretçiler ORDER BY id DESC LIMIT 10")
-       isimler = [row[0] for row in cur.fetchal()]
+       cur.execute("SELECT isim FROM ziyaretciler ORDER BY id DESC LIMIT 10")
+       isimler = [row[0] for row in cur.fetchall()]
 
        cur.close()
        conn.close()
        return  render_template_string(HTML, isimler=isimler)
 
-       if__name__=="__main__":
-            app.run(host="0.0.0.0", port=5000)
+if__name__=="__main__":
+   app.run(host="0.0.0.0", port=5000)
      
   
                  
