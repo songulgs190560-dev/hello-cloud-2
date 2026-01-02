f9e16db4883bb6b1720e1a2c67431539ec80b616
songulgs190560-dev <songulgs190560@gmail.com>
2026-01-02 18:43:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,23 @@
-from flask import Flask, render_template_string, request
 import os
 import psycopg2
+from flask import Flask
 
-app=Flask (__name__)
+app = Flask(__name__)
 
-DATABASE_URL = os.getenv("DATABASE_URL"," ")
-
-HTML = """
-<!doctype html>
-<html>
-<head>
-   <title>Buluttan Selam!</title>
-   <style>
-        body  {  font-family: Arial; text-align: center; padding: 50px; background: #eef2f3; }
-        h1    {  color:  #333;  }
-        form  {  margin:  20px auto;  }
-        input {padding:  10px; font-size:  16px;  }
-        button {padding:  10px  15px; background:  #4CAF50; color:white; border: none; border-radius: 6px; cursor: pointer; }
-        ul  { list-style: none;  padding: 0;  }
-        li  { background: white; margin: 5px auto; width: 200px; padding: 8px; border-radius: 5px; }
-   </style>
-</head>
-<body>
-  <h1>Buluttan Selam!</h1>
-  <p>Songül, Merhaba </p>
-  <form method="POST">
-            <input type="text" name="isim"  placeholder="Songül" required>
-            </form>
-            <h3>ziyaretçiler:</h3>
-            <ul>
-               {% for ad in isimler %}
-                 <li>{{ ad }}</li>
-               {% endfor %}
-           </ul>
-</body>
-</html>
-"""
-
+# Render'dan gelen DATABASE_URL
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def connect_db():
-  conn = psycopg2.connect(DATABASE_URL)
-  return conn
-@app.route("/", methods=["GET" , "POST"])
+    return psycopg2.connect(DATABASE_URL)
+
+@app.route("/")
 def index():
-  conn = connect_db()
-  cur = conn.cursor()
-  cur.execute("CREATE TABLE IF NOT EXISTS ziyaretciler (id SERIAL PRIMARY KEY, isim TEXT)")
-
-
-  if request.method=="POST":
-     isim = request.form.get("isim")
-     if isim:
-       cur.execute("INSERT INTO ziyaretciler (isim) VALUES (%s)", (isim,))
-       conn.commit()
-
-       cur.execute("SELECT isim FROM ziyaretciler ORDER BY id DESC LIMIT 10")
-       isimler = [row[0] for row in cur.fetchall()]
-
-       cur.close()
-       conn.close()
-       return  render_template_string(HTML, isimler=isimler)
+    conn = connect_db()
+    cur = conn.cursor()
+    cur.execute("SELECT 1;")
+    cur.close()
+    conn.close()
+    return "Veritabanı bağlantısı başarılı 🎉"
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=5000)
-     
-  
-                 
+    app.run(debug=True)
