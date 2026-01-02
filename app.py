7b73491814fee781d8270708ce906f006cdb2f3a
songulgs190560-dev <songulgs190560@gmail.com>
2026-01-02 18:51:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Render'dan gelen DATABASE_URL
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def connect_db():
+    if not DATABASE_URL:
+        return None
     return psycopg2.connect(DATABASE_URL)
 
 @app.route("/")
 def index():
+    if not DATABASE_URL:
+        return "Uygulama çalışıyor 🚀 (DATABASE_URL tanımlı değil)"
+
     conn = connect_db()
     cur = conn.cursor()
     cur.execute("SELECT 1;")
